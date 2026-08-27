231c88ed6e4e38b6aaa3d4e78188ae523d31c241
WattIQs <amaorbelo@gmail.com>
2026-08-26 23:32:06 -0300

    fix: keep lint focused on code errors

--- eslint.config.js
+++ eslint.config.js
@@ -36,10 +36,10 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
+  eslintPluginPrettier,
   {
     rules: {
       "prettier/prettier": "off",
     },
   },
-  eslintPluginPrettier,
 );
